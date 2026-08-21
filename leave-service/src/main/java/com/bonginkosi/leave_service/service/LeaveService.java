3231e7b9124a2adf688add05cbaadf5374d56842
Bonginkosi Tlou <pb.tlou@gmail.com>
2026-08-21 23:49:27 +0200

    Added POST method to create leave record for an employee, when an employee is created(in employee-service)

--- leave-service/src/main/java/com/bonginkosi/leave_service/service/LeaveService.java
+++ leave-service/src/main/java/com/bonginkosi/leave_service/service/LeaveService.java
@@ -3,9 +3,10 @@ package com.bonginkosi.leave_service.service;
 import com.bonginkosi.leave_service.dto.EmployeeDto;
 import com.bonginkosi.leave_service.dto.LeaveDto;
 import com.bonginkosi.leave_service.entity.Leave;
+import com.bonginkosi.leave_service.enums.LeaveStatus;
+import com.bonginkosi.leave_service.enums.LeaveType;
 import com.bonginkosi.leave_service.event.EmployeeCreatedEvent;
 import com.bonginkosi.leave_service.repository.LeaveRepository;
-import feign.FeignException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,37 +15,24 @@ public class LeaveService {
 
     @Autowired
     private final LeaveRepository leaveRepository;
-    private final EmployeeClient employeeClient;
-    public LeaveService(LeaveRepository leaveRepository, EmployeeClient employeeClient) {
+    public LeaveService(LeaveRepository leaveRepository) {
         this.leaveRepository = leaveRepository;
-        this.employeeClient = employeeClient;
     }
 
     //Method(POST) to create a leave record for an employee
-    public LeaveDto createLeave(LeaveDto leaveDto){
-        // Verify that the employee exists
-        EmployeeDto employee;
-
-        try {
-            employee = employeeClient.getEmployeeById(leaveDto.getEmployeeId());
-        } catch (FeignException.NotFound ex) {
-            throw new RuntimeException(
-                    "User with ID " + leaveDto.getEmployeeId() + " was not found."
-            );
-        }
-
-        //Creating a leave record for the employee
+    public void createLeave(EmployeeCreatedEvent event) {
         Leave leave = new Leave();
-        leave.setLeaveType(leaveDto.getLeaveType());
-        leave.setStartDate(leaveDto.getStartDate());
-        leave.setEndDate(leaveDto.getEndDate());
-        leave.setStatus(leaveDto.getStatus());
-        leave.setReason(leaveDto.getReason());
-        leave.setEmployeeId(employee.getId());
-        leaveRepository.save(leave);
 
-        return leaveDto;
+        // Link the employee to the leave record
+        leave.setEmployeeId(event.getId());
+
+        // Leave information will be added later using PATCH
+        leaveRepository.save(leave);
     }
+
+
+
+
 
 }
 
